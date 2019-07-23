cb0674829ae9c606911b6fdc2aa3a526d3ae4395
Jose Luis Cadena <=>
2019-07-22 23:48:02 -0500

    complex migration, database version 3

--- app/src/main/kotlin/user/data/repository/UserRepository.kt
+++ app/src/main/kotlin/user/data/repository/UserRepository.kt
@@ -6,7 +6,7 @@ import user.data.datasource.room.UserRoomDataSource
 class UserRepository(private val userRoomDataSource: UserRoomDataSource) {
 
     fun insertUser(accountId: String, userName: String, email: String) =
-        userRoomDataSource.insert(UserEntity(accountId = accountId, userName = userName, email = email))
+        userRoomDataSource.insert(UserEntity(accountId = accountId.toInt(), userName = userName, email = email))
 
     fun getUsers() = userRoomDataSource.getAll()
 }
