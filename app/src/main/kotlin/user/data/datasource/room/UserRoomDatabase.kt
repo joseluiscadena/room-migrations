a02c536da81fdebc7d30d1a0c24a8706712cfce9
Jose Luis Cadena <joseph.dev89@gmail.com>
2019-07-31 14:50:02 -0500

    multiple version migration 1 to 3

--- app/src/main/kotlin/user/data/datasource/room/UserRoomDatabase.kt
+++ app/src/main/kotlin/user/data/datasource/room/UserRoomDatabase.kt
@@ -16,7 +16,7 @@ abstract class UserRoomDatabase : RoomDatabase() {
 
     companion object {
         fun build(context: Context) = Room.databaseBuilder(context, UserRoomDatabase::class.java, DATABASE_USER)
-            .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
+            .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_1_3)
             .build()
     }
 }
@@ -37,6 +37,20 @@ val MIGRATION_2_3: Migration = object : Migration(2, 3) {
                     "phone TEXT NOT NULL)")
             execSQL("INSERT INTO user_new (account_id, username, email, address, phone)" +
                     "SELECT 1, username, email, '', phone FROM user")
+            execSQL("DROP TABLE user")
+            execSQL("ALTER TABLE user_new RENAME TO user")
+        }
+    }
+}
+
+val MIGRATION_1_3: Migration = object : Migration(1, 3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.run {
+            execSQL("CREATE TABLE user_new (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    "account_id INTEGER NOT NULL, username TEXT NOT NULL, email TEXT NOT NULL, address TEXT NOT NULL," +
+                    "phone TEXT NOT NULL)")
+            execSQL("INSERT INTO user_new (account_id, username, email, address, phone)" +
+                    "SELECT 1, username, email, 'periferico', '55' FROM user")
             execSQL("DROP TABLE user")
             execSQL("ALTER TABLE user_new RENAME TO user")
         }
